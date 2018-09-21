ad3c680ccbd739c76d56f4490328120cba6812ae
Lzy <lzy2359438@163.com>
2018-09-21 17:04:39 +0800

    呃...问题修正(TypeNotPresentExceptionProxy)#Lzy
    在SpringBoot项目的启动类中排除SecurityAutoConfiguration

--- Project/novaSupermarketInvoicingSystem/src/main/java/com/invoicingSystem/main/NovaSupermarketInvoicingSystemApplication.java
+++ Project/novaSupermarketInvoicingSystem/src/main/java/com/invoicingSystem/main/NovaSupermarketInvoicingSystemApplication.java
@@ -1,9 +1,11 @@
 package com.invoicingSystem.main;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class NovaSupermarketInvoicingSystemApplication {
 
 	public static void main(String[] args) {
