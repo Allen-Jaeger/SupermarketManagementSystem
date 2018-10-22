ca93384256cc27531d569e65b05b6c6a447c382f
wumao555 <381740656@qq.com>
2018-10-22 17:50:38 +0800

    indent工作流基本实现

---
+++ Project/novaSupermarketInvoicingSystem/src/main/java/com/invoicingSystem/main/indent/listener/GoodsCheckingEndListener.java
@@ -0,0 +1,50 @@
+package com.invoicingSystem.main.indent.listener;
+
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.invoicingSystem.main.indent.domain.Indent;
+import com.invoicingSystem.main.indent.service.IIndentService;
+import com.invoicingSystem.main.indent.util.IndentStatus;
+
+import java.util.Date;
+
+/**
+ * 销假后处理器
+ * <p>设置销假时间</p>
+ * <p>使用Spring代理，可以注入Bean，管理事物</p>
+ * bean  id=reportBackEndProcessor
+ */
+@Component
+@Transactional
+public class GoodsCheckingEndListener implements TaskListener 
+{
+	private static final long serialVersionUID = -8360605214753688651L;
+
+	@Autowired
+    private IIndentService indentService;
+
+    @Autowired
+    private RuntimeService runtimeService;
+    
+    public void notify(DelegateTask delegateTask) {
+    	
+        String processInstanceId = delegateTask.getProcessInstanceId();
+        ProcessInstance processInstance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        Indent indent = indentService.findById(new Long(processInstance.getBusinessKey()));
+
+        if(delegateTask.getVariable("examinationPass").toString() =="true") {
+        	indent.setIndentStatus(IndentStatus.FINISHED);
+        }else {
+        	indent.setIndentStatus(IndentStatus.ERROR);
+        }
+        
+        indent.setGoodsCheckingReason(delegateTask.getVariable("goodsCheckingReason").toString());
+    }
+}
---
+++ Project/novaSupermarketInvoicingSystem/supermarketInvoicingSystem/classic/src/view/process/indent/IndentProcessGoodsCheck.js
@@ -0,0 +1,64 @@
+﻿Ext.define('SupermarketInvoicingSystem.view.process.indent.IndentProcessGoodsCheck', {
+    extend:'Ext.window.Window',
+  
+    alias: 'widget.indentProcessGoodsCheck',
+    id:'indentProcessGoodsCheck',
+    height: 150,
+    minHeight: 350,
+    minWidth: 300,
+    width: 220,
+    scrollable: true,
+    resizable:true,
+    title: 'Indent Detail',
+    closable: true,
+    constrain: true,
+    defaultFocus: 'textfield',
+    modal:true,
+    layout: 'fit',
+    requires:[
+      'Ext.form.RadioGroup'
+    ],
+    
+    
+    defaults: {
+        bodyPadding: 10
+   },
+    items: [{
+     scrollable: true,
+        xtype: 'form',
+        layout: 'form',
+        padding: '10px',
+        ariaLabel: 'Enter your name',
+        id:'GoodsCheck',
+    
+    items: [
+    
+      
+        {xtype: 'textfield',fieldLabel: 'id',name:'id',id:"indentId",hidden: true},
+        {xtype: 'textfield',fieldLabel: 'taskId',name:'taskId',id:"taskId",hidden: true},
+        {xtype: 'textfield',fieldLabel: 'taskName',name:'taskName',id:"taskName",hidden: true},
+        {xtype: 'radiogroup',fieldLabel: '审批',labelWidth:50,
+            items:[
+              {boxLabel:'同意',name:'check',inputValue:true,width:70,checked:true},
+              {boxLabel:'不同意',name:'check',inputValue:false,width:100}
+            ]
+          },
+        {xtype: 'textareafield',fieldLabel: '备注',name:'reason',labelWidth:50,width:350,height:150,grow:false}  
+        
+        ]
+    }]
+,
+  buttons: ['->',{
+        xtype: 'button',
+        text: '提交',
+        iconCls: 'fa fa-search',
+        handler: 'submitGoodsCheckWindow'
+    },{
+        xtype: 'button',
+        text: '关闭',
+        handler: function(btn) {
+            Ext.getCmp('leftList').getStore().removeAll();
+            btn.up('window').close();
+        }
+    },'->']
+});
